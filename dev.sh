9da1910c56bd77e7024091ee5675da4465bf56d2
NovrinAulia <novrinaulia@gmail.com>
2025-12-19 18:03:57 +0700

    add dev.sh for automatically run server and npm dev

---
+++ dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Menjalankan Laravel Dev Environment..."
+
+# Menjalankan PHP Artisan Serve di jendela baru
+mintty -e php artisan serve &
+
+# Menjalankan NPM Run Dev di jendela baru
+mintty -e npm run dev &
+
+# Menjalankan Queue Worker di jendela baru
+mintty -e php artisan queue:listen --tries=1 &
+
+echo "Semua proses telah dijalankan di jendela terpisah!"
