84680c84e28cc7f24b10203f4fd20e22e9dae7ac
NovrinAulia <novrinaulia@gmail.com>
2025-12-19 18:03:17 +0700

    add welcome greeting to user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (Auth::user()->isAdmin())
+                        <h1>Welcome Admin {{ Auth::user()->name }}!</h1>
+                        <p>You have full access to the inventory management system.</p>
+                    @else
+                        <h1>Welcome Staff {{ Auth::user()->name }}!</h1>
+                        <p>You have limited access to the inventory management system.</p>
+                    @endif
                 </div>
             </div>
         </div>
